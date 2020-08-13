5525bf18114237ea0ab2c612d8c1c1b92f964737
Matt Leuer <mattleuer@gmail.com>
2020-08-12 17:33:36 -0700

    Update portfolio page to include technologies used in projects

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -6,92 +6,11 @@ function Portfolio() {
     <div className="portfolio-container">
       <section className="cards">
         <div className="card">
-          <a href="https://trackfood.herokuapp.com/" target="_blank" rel="noopener noreferrer">
-            <div className="card-image-container">
-              <img
-                src={require("../../assets/Images/FoodTrak.jpeg")}
-                alt="project 1"
-              ></img>
-            </div>
-            <div className="card-content">
-              <p className="card-title text-medium">FoodTrak</p>
-              <div className="card-info">
-                <p className="text-medium">Calorie tracking</p>
-              </div>
-            </div>
-          </a>
-        </div>
-
-        <div className="card">
-          <a href="https://mleuer.github.io/Project-1/" target="_blank" rel="noopener noreferrer">
-            <div className="card-image-container">
-              <img
-                src={require("../../assets/Images/basketball.jpg")}
-                alt="project 2"
-              ></img>
-            </div>
-            <div className="card-content">
-              <p className="card-title text-medium">Basketball Collect 'em All</p>
-              <div className="card-info">
-                <p className="text-medium">Basketball card collecting</p>
-              </div>
-            </div>
-          </a>
-        </div>
-
-        <div className="card">
-          <a href="https://mleuer.github.io/Weather-Dashboard/" target="_blank" rel="noopener noreferrer">
-            <div className="card-image-container">
-              <img
-                src={require("../../assets/Images/weather-dashboard.png")}
-                alt="project 3"
-              ></img>
-            </div>
-            <div className="card-content">
-              <p className="card-title text-medium">Weather Dashboard</p>
-              <div className="card-info">
-                <p className="text-medium">Search weather by city </p>
-              </div>
-            </div>
-          </a>
-        </div>
-
-        <div className="card">
-          <a href="https://radiant-taiga-33936.herokuapp.com/" target="_blank" rel="noopener noreferrer">
-            <div className="card-image-container">
-              <img
-                src={"https://illustoon.com/photo/4728.png"}
-                alt="project 4"
-              ></img>
-            </div>
-            <div className="card-content">
-              <p className="card-title text-medium">Burger Finder</p>
-              <div className="card-info">
-                <p className="text-medium">Add and devour burgers </p>
-              </div>
-            </div>
-          </a>
-        </div>
-
-        <div className="card">
-          <a href="https://trackfood.herokuapp.com/" target="_blank" rel="noopener noreferrer">
-            <div className="card-image-container">
-              <img
-                src={require("../../assets/Images/work-day-scheduler.png")}
-                alt="project 6"
-              ></img>
-            </div>
-            <div className="card-content">
-              <p className="card-title text-medium">Work Day Scheduler</p>
-              <div className="card-info">
-                <p className="text-medium">Track daily tasks</p>
-              </div>
-            </div>
-          </a>
-        </div>
-
-        <div className="card">
-          <a href="https://color-story.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://color-story.herokuapp.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <div className="card-image-container">
               <img
                 src={require("../../assets/Images/colorstory.png")}
@@ -101,12 +20,176 @@ function Portfolio() {
             <div className="card-content">
               <p className="card-title text-medium">Color Story</p>
               <div className="card-info">
-                <p className="text-medium">Social media for artists</p>
+                <ul>
+                  <li>
+                    <p className="text-medium">-Social Networking for Artists and Enthusiasts</p>
+                  </li>
+                  <li>
+                    <p className="text-medium">
+                      -Technologies: React.js Express.js, Sequelize, MaterialUI, Axios
+                    </p>
+                  </li>
+                </ul>
               </div>
             </div>
           </a>
         </div>
 
+        <div className="card">
+          <a
+            href="https://trackfood.herokuapp.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="card-image-container">
+              <img
+                src={require("../../assets/Images/FoodTrak.jpeg")}
+                alt="project 1"
+              ></img>
+            </div>
+            <div className="card-content">
+              <p className="card-title text-medium">FoodTrak</p>
+              <div className="card-info">
+                <ul>
+                  <li>
+                    <p className="text-medium">-Calorie tracking app</p>
+                  </li>
+                  <li>
+                    <p className="text-medium">
+                      -Technologies: Express.js, MySQL, Sequelize, Bootstrap
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div className="card">
+          <a
+            href="https://mleuer.github.io/Project-1/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="card-image-container">
+              <img
+                src={require("../../assets/Images/basketball.jpg")}
+                alt="project 2"
+              ></img>
+            </div>
+            <div className="card-content">
+              <p className="card-title text-medium">
+                Basketball Collect 'em All
+              </p>
+              <div className="card-info">
+                <ul>
+                  <li>
+                    <p className="text-medium">
+                      -Simple Basketball Card Collecting Game
+                    </p>
+                  </li>
+                  <li>
+                    <p className="text-medium">
+                      -Technologies: Bootstrap, JQuery
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div className="card">
+          <a
+            href="https://mleuer.github.io/Weather-Dashboard/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="card-image-container">
+              <img
+                src={require("../../assets/Images/weather-dashboard.png")}
+                alt="project 3"
+              ></img>
+            </div>
+            <div className="card-content">
+              <p className="card-title text-medium">Weather Dashboard</p>
+              <div className="card-info">
+                <ul>
+                  <li>
+                    <p className="text-medium">
+                      -Real-Time Searchable Weather Data
+                    </p>
+                  </li>
+                  <li>
+                    <p className="text-medium">
+                      -Technologies: OpenWeather API, HTML, JQuery, CSS
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div className="card">
+          <a
+            href="https://radiant-taiga-33936.herokuapp.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="card-image-container">
+              <img
+                src={"https://illustoon.com/photo/4728.png"}
+                alt="project 4"
+              ></img>
+            </div>
+            <div className="card-content">
+              <p className="card-title text-medium">Burger Finder</p>
+              <div className="card-info">
+                <ul>
+                  <li>
+                    <p className="text-medium">-Create and Store Burgers</p>
+                  </li>
+                  <li>
+                    <p className="text-medium">
+                      -Technologies: MySQL, Express.js, Express-Handlebars
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div className="card">
+          <a
+            href="https://trackfood.herokuapp.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="card-image-container">
+              <img
+                src={require("../../assets/Images/work-day-scheduler.png")}
+                alt="project 6"
+              ></img>
+            </div>
+            <div className="card-content">
+              <p className="card-title text-medium">Work Day Scheduler</p>
+              <div className="card-info">
+                <ul>
+                  <li>
+                    <p className="text-medium">-Track Daily Tasks</p>
+                  </li>
+                  <li>
+                    <p className="text-medium">
+                      -Technologies: Moment.js, HTML, CSS
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </a>
+        </div>
       </section>
     </div>
   );
